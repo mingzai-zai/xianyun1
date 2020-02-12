26d051d6e5a1b6f3b6cf33042cac240afffdaf27
mingzai-zai <mingzai_2020@163.com>
2020-02-12 15:46:53 +0800

    航班列表动态渲染完成

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -10,7 +10,7 @@
         <FlightsListHeader />
 
         <!-- 航班信息 -->
-        <FlightsInfo />
+        <FlightsInfo v-for='(e,i) in flightslist' :key="i" :data='e'/>
       </div>
 
       <!-- 侧边栏 -->
@@ -27,7 +27,26 @@ import FlightsInfo from "@/components/air/flightsInfo";
 
 export default {
   data() {
-    return {};
+    return {
+      flightsdata:{},//总数据
+      flightslist:[],//航班列表
+    };
+  },
+  mounted(){
+    // console.log(this.$route)去query的参数有相关URL都可以打印出来看看
+    //就是从路由里面那数据
+    // query的数据刚好是要传的数据
+    this.$axios({
+      url:'/airs',
+      params:this.$route.query
+    }).then(res=>{
+      // console.log(res);
+      // data包含着flights，info，options，total四个数据
+      let {data} =res;
+      this.flightsdata=data;
+      //先全部存起来后面就可以方便用，不然也可以设置四个数据一个一个接收
+      this.flightslist=this.flightsdata.flights;
+    })
   },
   components: {
     FlightsListHeader,FlightsInfo
